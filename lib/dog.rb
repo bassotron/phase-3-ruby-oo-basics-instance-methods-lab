727386e9b4ab5b6b01b439e64f216119d10dda35
Will Lovell <willard.lovell@icloud.com>
2022-08-31 15:56:03 -0600

    commit tree 6abb524f6cb518c4001502fa4b352f6dea295411

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+	def bark
+		puts "Woof!"
+	def sit
+       puts "The Dog is sitting"
+		end
+		
+	end
+	
+end
+
+
+fido = Dog.new
+snoopy = Dog.new
+fido.bark
+snoopy.bark
+fido.sit
+snoopy.sit
---
+++ lib/person.rb
@@ -0,0 +1,12 @@
+class Person 
+def talk 
+	puts "Hello World!"
+def walk 
+	puts "The Person is walking"
+    end
+  end 	
+end
+
+Bill = Person.new
+Bill.talk 
+Bill.walk 
